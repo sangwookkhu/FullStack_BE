ce93540121f6fcd47ec97c8ab144ed816dd940c1
강상욱 <k_sw@gangsang-ug-ui-noteubug.local>
2025-01-06 23:30:45 +0900

    mainpage, loginpage

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ const port = 8080;
 
 const app = express();
 
-app.get('/', (req, res) => {
-  console.log('this is main page');
+app.get('/login', (req, res) => {
+  console.log('this is login page');
 });
+
+app.get('/main', (req, res) => {
+  console.log('this is main page');
+})
 
 app.listen(port, (req, res) => {
   console.log(`App is running at port ${port}`);
